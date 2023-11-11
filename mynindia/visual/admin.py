7b89fc5e25514e38a93634858a0d5227bd055291
nindiappc <148948592+nindiappc@users.noreply.github.com>
2023-11-12 00:07:32 +0700

    Delete mynindia directory

--- mynindia/visual/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import List_Pembelian, Product
-# Register your models here.
-
-class List_PembelianAdmin(admin.ModelAdmin):
-    list_display = ("jumlah", "alamat_tujuan", "No_HP",)
-
-admin.site.register(List_Pembelian, List_PembelianAdmin)
-admin.site.register(Product)
