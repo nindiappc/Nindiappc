fd47219c5d04d81ba129319777409d758cbdef5a
nindiappc <148948592+nindiappc@users.noreply.github.com>
2023-11-12 00:06:58 +0700

    Add files via upload

---
+++ mynindia/visual/static/js/musicku.js
@@ -0,0 +1,74 @@
+const previous = document.querySelector('#previous');
+const play = document.querySelector('#play');
+const next = document.querySelector('#next');
+
+const title = document.querySelector('#title');
+const image = document.querySelector('#image');
+
+const track = document.createElement('audio');
+
+let index = 0;
+let playingSong = false;
+
+let songs = [{
+        name: 'Spine Breaker',
+        path: url('./songs/Spine Breaker.mp3'),
+        image: url('./images/spine.jpg'),
+    },
+];
+
+function loadTrack(index) {
+    track.src = songs[index].path;
+    title.innerHTML = songs[index].name;
+    image.src = songs[index].image;
+    track.load();
+}
+
+loadTrack(index);
+
+function playSong() {
+    track.play();
+    playingSong = true;
+    play.innerHTML = '<i class="fas fa-pause"></i>';
+}
+
+function pauseSong() {
+    track.pause();
+    playingSong = false;
+    play.innerHTML = '<i class="fas fa-play"></i>';
+}
+
+function nextSong() {
+    if (index < songs.length - 1) {
+        index += 1;
+        loadTrack(index);
+        playSong();
+    } else {
+        index = 0;
+        loadTrack(index);
+        playSong();
+
+    }
+}
+
+function previousSong() {
+    if (index > 0) {
+        index -= 1;
+        loadTrack(index);
+        playSong();
+
+    } else {
+        index = songs.length;
+        loadTrack(index);
+        playSong();
+    }
+}
+
+function justPlay() {
+    if (playingSong == false) {
+        playSong();
+
+    } else {
+        pauseSong();
+    }
+}
